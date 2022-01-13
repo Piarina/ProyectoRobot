62ac978c7d28881b32afee6c6b907d93437e2a62
Piarina <97671614+Piarina@users.noreply.github.com>
2022-01-13 12:17:17 +0100

    Delete Unificado_rover_final directory

--- Unificado_rover_final/VL53L0X_rasp_python/python/prueba_medida_CPU.py
+++
@@ -1,8 +0,0 @@
-import os 
-# Retorna la temperatura de la CPU de la RPi como float
-def measure_temp():
-    temp = os.popen('vcgencmd measure_temp').readline() # función que hace todo
-    print(temp)
-    return(temp.replace("temp=","").replace("'C\n",""))
-
-print(float(measure_temp()))
